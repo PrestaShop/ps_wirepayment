8f972d7985929d1ff5553a016ce279db19bed59f
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-04-29 16:42:19 +0200

    // Version updated to v1.0.6

--- bankwire.php
+++ bankwire.php
@@ -40,7 +40,7 @@ class BankWire extends PaymentModule
 	{
 		$this->name = 'bankwire';
 		$this->tab = 'payments_gateways';
-		$this->version = '1.0.5';
+		$this->version = '1.0.6';
 		$this->author = 'PrestaShop';
 		$this->controllers = array('payment', 'validation');
 		$this->is_eu_compatible = 1;
