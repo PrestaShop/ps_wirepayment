47367e4139c6e9b2ee0b541b9c71b5bbda2c216d
Maxime Biloé <maxime.biloe@prestashop.com>
2015-11-03 14:33:01 +0100

    //norm

--- bankwire.php
+++ bankwire.php
@@ -24,266 +24,282 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
-	exit;
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 class BankWire extends PaymentModule
 {
-	protected $_html = '';
-	protected $_postErrors = array();
+    protected $_html = '';
+    protected $_postErrors = array();
 
-	public $details;
-	public $owner;
-	public $address;
-	public $extra_mail_vars;
-	public function __construct()
-	{
-		$this->name = 'bankwire';
-		$this->tab = 'payments_gateways';
-		$this->version = '2.0.0';
-		$this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
-		$this->author = 'PrestaShop';
-		$this->controllers = array('payment', 'validation');
-		$this->is_eu_compatible = 1;
+    public $details;
+    public $owner;
+    public $address;
+    public $extra_mail_vars;
 
-		$this->currencies = true;
-		$this->currencies_mode = 'checkbox';
+    public function __construct()
+    {
+        $this->name = 'bankwire';
+        $this->tab = 'payments_gateways';
+        $this->version = '2.0.0';
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->author = 'PrestaShop';
+        $this->controllers = array('payment', 'validation');
+        $this->is_eu_compatible = 1;
 
-		$config = Configuration::getMultiple(array('BANK_WIRE_DETAILS', 'BANK_WIRE_OWNER', 'BANK_WIRE_ADDRESS'));
-		if (!empty($config['BANK_WIRE_OWNER']))
-			$this->owner = $config['BANK_WIRE_OWNER'];
-		if (!empty($config['BANK_WIRE_DETAILS']))
-			$this->details = $config['BANK_WIRE_DETAILS'];
-		if (!empty($config['BANK_WIRE_ADDRESS']))
-			$this->address = $config['BANK_WIRE_ADDRESS'];
+        $this->currencies = true;
+        $this->currencies_mode = 'checkbox';
 
-		$this->bootstrap = true;
-		parent::__construct();
+        $config = Configuration::getMultiple(array('BANK_WIRE_DETAILS', 'BANK_WIRE_OWNER', 'BANK_WIRE_ADDRESS'));
+        if (!empty($config['BANK_WIRE_OWNER'])) {
+            $this->owner = $config['BANK_WIRE_OWNER'];
+        }
+        if (!empty($config['BANK_WIRE_DETAILS'])) {
+            $this->details = $config['BANK_WIRE_DETAILS'];
+        }
+        if (!empty($config['BANK_WIRE_ADDRESS'])) {
+            $this->address = $config['BANK_WIRE_ADDRESS'];
+        }
 
-		$this->displayName = $this->l('Bank wire');
-		$this->description = $this->l('Accept payments for your products via bank wire transfer.');
-		$this->confirmUninstall = $this->l('Are you sure about removing these details?');
-		if (!isset($this->owner) || !isset($this->details) || !isset($this->address))
-			$this->warning = $this->l('Account owner and account details must be configured before using this module.');
-		if (!count(Currency::checkPaymentCurrencies($this->id)))
-			$this->warning = $this->l('No currency has been set for this module.');
+        $this->bootstrap = true;
+        parent::__construct();
 
-		$this->extra_mail_vars = array(
-										'{bankwire_owner}' => Configuration::get('BANK_WIRE_OWNER'),
-										'{bankwire_details}' => nl2br(Configuration::get('BANK_WIRE_DETAILS')),
-										'{bankwire_address}' => nl2br(Configuration::get('BANK_WIRE_ADDRESS'))
-										);
-	}
+        $this->displayName = $this->l('Bank wire');
+        $this->description = $this->l('Accept payments for your products via bank wire transfer.');
+        $this->confirmUninstall = $this->l('Are you sure about removing these details?');
+        if (!isset($this->owner) || !isset($this->details) || !isset($this->address)) {
+            $this->warning = $this->l('Account owner and account details must be configured before using this module.');
+        }
+        if (!count(Currency::checkPaymentCurrencies($this->id))) {
+            $this->warning = $this->l('No currency has been set for this module.');
+        }
 
-	public function install()
-	{
-		if (!parent::install() || !$this->registerHook('payment') || ! $this->registerHook('displayPaymentEU') || !$this->registerHook('paymentReturn'))
-			return false;
-		return true;
-	}
+        $this->extra_mail_vars = array(
+                                        '{bankwire_owner}' => Configuration::get('BANK_WIRE_OWNER'),
+                                        '{bankwire_details}' => nl2br(Configuration::get('BANK_WIRE_DETAILS')),
+                                        '{bankwire_address}' => nl2br(Configuration::get('BANK_WIRE_ADDRESS'))
+                                        );
+    }
 
-	public function uninstall()
-	{
-		if (!Configuration::deleteByName('BANK_WIRE_DETAILS')
-				|| !Configuration::deleteByName('BANK_WIRE_OWNER')
-				|| !Configuration::deleteByName('BANK_WIRE_ADDRESS')
-				|| !parent::uninstall())
-			return false;
-		return true;
-	}
+    public function install()
+    {
+        if (!parent::install() || !$this->registerHook('payment') || ! $this->registerHook('displayPaymentEU') || !$this->registerHook('paymentReturn')) {
+            return false;
+        }
+        return true;
+    }
 
-	protected function _postValidation()
-	{
-		if (Tools::isSubmit('btnSubmit'))
-		{
-			if (!Tools::getValue('BANK_WIRE_DETAILS'))
-				$this->_postErrors[] = $this->l('Account details are required.');
-			elseif (!Tools::getValue('BANK_WIRE_OWNER'))
-				$this->_postErrors[] = $this->l('Account owner is required.');
-		}
-	}
+    public function uninstall()
+    {
+        if (!Configuration::deleteByName('BANK_WIRE_DETAILS')
+                || !Configuration::deleteByName('BANK_WIRE_OWNER')
+                || !Configuration::deleteByName('BANK_WIRE_ADDRESS')
+                || !parent::uninstall()) {
+            return false;
+        }
+        return true;
+    }
 
-	protected function _postProcess()
-	{
-		if (Tools::isSubmit('btnSubmit'))
-		{
-			Configuration::updateValue('BANK_WIRE_DETAILS', Tools::getValue('BANK_WIRE_DETAILS'));
-			Configuration::updateValue('BANK_WIRE_OWNER', Tools::getValue('BANK_WIRE_OWNER'));
-			Configuration::updateValue('BANK_WIRE_ADDRESS', Tools::getValue('BANK_WIRE_ADDRESS'));
-		}
-		$this->_html .= $this->displayConfirmation($this->l('Settings updated'));
-	}
+    protected function _postValidation()
+    {
+        if (Tools::isSubmit('btnSubmit')) {
+            if (!Tools::getValue('BANK_WIRE_DETAILS')) {
+                $this->_postErrors[] = $this->l('Account details are required.');
+            } elseif (!Tools::getValue('BANK_WIRE_OWNER')) {
+                $this->_postErrors[] = $this->l('Account owner is required.');
+            }
+        }
+    }
 
-	protected function _displayBankWire()
-	{
-		return $this->display(__FILE__, 'infos.tpl');
-	}
+    protected function _postProcess()
+    {
+        if (Tools::isSubmit('btnSubmit')) {
+            Configuration::updateValue('BANK_WIRE_DETAILS', Tools::getValue('BANK_WIRE_DETAILS'));
+            Configuration::updateValue('BANK_WIRE_OWNER', Tools::getValue('BANK_WIRE_OWNER'));
+            Configuration::updateValue('BANK_WIRE_ADDRESS', Tools::getValue('BANK_WIRE_ADDRESS'));
+        }
+        $this->_html .= $this->displayConfirmation($this->l('Settings updated'));
+    }
 
-	public function getContent()
-	{
-		if (Tools::isSubmit('btnSubmit'))
-		{
-			$this->_postValidation();
-			if (!count($this->_postErrors))
-				$this->_postProcess();
-			else
-				foreach ($this->_postErrors as $err)
-					$this->_html .= $this->displayError($err);
-		}
-		else
-			$this->_html .= '<br />';
+    protected function _displayBankWire()
+    {
+        return $this->display(__FILE__, 'infos.tpl');
+    }
 
-		$this->_html .= $this->_displayBankWire();
-		$this->_html .= $this->renderForm();
+    public function getContent()
+    {
+        if (Tools::isSubmit('btnSubmit')) {
+            $this->_postValidation();
+            if (!count($this->_postErrors)) {
+                $this->_postProcess();
+            } else {
+                foreach ($this->_postErrors as $err) {
+                    $this->_html .= $this->displayError($err);
+                }
+            }
+        } else {
+            $this->_html .= '<br />';
+        }
 
-		return $this->_html;
-	}
+        $this->_html .= $this->_displayBankWire();
+        $this->_html .= $this->renderForm();
 
-	public function hookPayment($params)
-	{
-		if (!$this->active)
-			return;
-		if (!$this->checkCurrency($params['cart']))
-			return;
+        return $this->_html;
+    }
 
-		$this->smarty->assign(array(
-			'this_path' => $this->_path,
-			'this_path_bw' => $this->_path,
-			'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
-		));
-		return $this->display(__FILE__, 'payment.tpl');
-	}
+    public function hookPayment($params)
+    {
+        if (!$this->active) {
+            return;
+        }
+        if (!$this->checkCurrency($params['cart'])) {
+            return;
+        }
 
-	public function hookDisplayPaymentEU($params)
-	{
-		if (!$this->active)
-			return;
+        $this->smarty->assign(array(
+            'this_path' => $this->_path,
+            'this_path_bw' => $this->_path,
+            'this_path_ssl' => Tools::getShopDomainSsl(true, true).__PS_BASE_URI__.'modules/'.$this->name.'/'
+        ));
+        return $this->display(__FILE__, 'payment.tpl');
+    }
 
-		if (!$this->checkCurrency($params['cart']))
-			return;
+    public function hookDisplayPaymentEU($params)
+    {
+        if (!$this->active) {
+            return;
+        }
 
-		$payment_options = array(
-			'cta_text' => $this->l('Pay by Bank Wire'),
-			'logo' => Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/bankwire.jpg'),
-			'action' => $this->context->link->getModuleLink($this->name, 'validation', array(), true)
-		);
+        if (!$this->checkCurrency($params['cart'])) {
+            return;
+        }
 
-		return $payment_options;
-	}
+        $payment_options = array(
+            'cta_text' => $this->l('Pay by Bank Wire'),
+            'logo' => Media::getMediaPath(_PS_MODULE_DIR_.$this->name.'/bankwire.jpg'),
+            'action' => $this->context->link->getModuleLink($this->name, 'validation', array(), true)
+        );
 
-	public function hookPaymentReturn($params)
-	{
-		if (!$this->active)
-			return;
+        return $payment_options;
+    }
 
-		$state = $params['objOrder']->getCurrentState();
-		if (in_array($state, array(Configuration::get('PS_OS_BANKWIRE'), Configuration::get('PS_OS_OUTOFSTOCK'), Configuration::get('PS_OS_OUTOFSTOCK_UNPAID'))))
-		{
-			$bankwireOwner = $this->owner;
-			if (!$bankwireOwner) {
-				$bankwireOwner = '___________';
-			}
+    public function hookPaymentReturn($params)
+    {
+        if (!$this->active) {
+            return;
+        }
 
-			$bankwireDetails = Tools::nl2br($this->details);
-			if (!$bankwireDetails) {
-				$bankwireDetails = '___________';
-			}
+        $state = $params['objOrder']->getCurrentState();
+        if (in_array($state, array(Configuration::get('PS_OS_BANKWIRE'), Configuration::get('PS_OS_OUTOFSTOCK'), Configuration::get('PS_OS_OUTOFSTOCK_UNPAID')))) {
+            $bankwireOwner = $this->owner;
+            if (!$bankwireOwner) {
+                $bankwireOwner = '___________';
+            }
 
-			$bankwireAddress = Tools::nl2br($this->address);
-			if (!$bankwireAddress) {
-				$bankwireAddress = '___________';
-			}
+            $bankwireDetails = Tools::nl2br($this->details);
+            if (!$bankwireDetails) {
+                $bankwireDetails = '___________';
+            }
 
-			$this->smarty->assign(array(
-				'shop_name' => $this->context->shop->name,
-				'total' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
-				'bankwireDetails' => $bankwireDetails,
-				'bankwireAddress' => $bankwireAddress,
-				'bankwireOwner' => $bankwireOwner,
-				'status' => 'ok',
-				'reference' => $params['objOrder']->reference,
-				'contact_url' => $this->context->link->getPageLink('contact', true)
-			));
-		}
-		else
-			$this->smarty->assign('status', 'failed');
-		return $this->display(__FILE__, 'payment_return.tpl');
-	}
+            $bankwireAddress = Tools::nl2br($this->address);
+            if (!$bankwireAddress) {
+                $bankwireAddress = '___________';
+            }
 
-	public function checkCurrency($cart)
-	{
-		$currency_order = new Currency($cart->id_currency);
-		$currencies_module = $this->getCurrency($cart->id_currency);
+            $this->smarty->assign(array(
+                'shop_name' => $this->context->shop->name,
+                'total' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
+                'bankwireDetails' => $bankwireDetails,
+                'bankwireAddress' => $bankwireAddress,
+                'bankwireOwner' => $bankwireOwner,
+                'status' => 'ok',
+                'reference' => $params['objOrder']->reference,
+                'contact_url' => $this->context->link->getPageLink('contact', true)
+            ));
+        } else {
+            $this->smarty->assign('status', 'failed');
+        }
+        return $this->display(__FILE__, 'payment_return.tpl');
+    }
 
-		if (is_array($currencies_module))
-			foreach ($currencies_module as $currency_module)
-				if ($currency_order->id == $currency_module['id_currency'])
-					return true;
-		return false;
-	}
+    public function checkCurrency($cart)
+    {
+        $currency_order = new Currency($cart->id_currency);
+        $currencies_module = $this->getCurrency($cart->id_currency);
 
-	public function renderForm()
-	{
-		$fields_form = array(
-			'form' => array(
-				'legend' => array(
-					'title' => $this->l('Contact details'),
-					'icon' => 'icon-envelope'
-				),
-				'input' => array(
-					array(
-						'type' => 'text',
-						'label' => $this->l('Account owner'),
-						'name' => 'BANK_WIRE_OWNER',
-						'required' => true
-					),
-					array(
-						'type' => 'textarea',
-						'label' => $this->l('Details'),
-						'name' => 'BANK_WIRE_DETAILS',
-						'desc' => $this->l('Such as bank branch, IBAN number, BIC, etc.'),
-						'required' => true
-					),
-					array(
-						'type' => 'textarea',
-						'label' => $this->l('Bank address'),
-						'name' => 'BANK_WIRE_ADDRESS',
-						'required' => true
-					),
-				),
-				'submit' => array(
-					'title' => $this->l('Save'),
-				)
-			),
-		);
+        if (is_array($currencies_module)) {
+            foreach ($currencies_module as $currency_module) {
+                if ($currency_order->id == $currency_module['id_currency']) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
-		$helper = new HelperForm();
-		$helper->show_toolbar = false;
-		$helper->table = $this->table;
-		$lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
-		$helper->default_form_language = $lang->id;
-		$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
-		$this->fields_form = array();
-		$helper->id = (int)Tools::getValue('id_carrier');
-		$helper->identifier = $this->identifier;
-		$helper->submit_action = 'btnSubmit';
-		$helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
-		$helper->token = Tools::getAdminTokenLite('AdminModules');
-		$helper->tpl_vars = array(
-			'fields_value' => $this->getConfigFieldsValues(),
-			'languages' => $this->context->controller->getLanguages(),
-			'id_language' => $this->context->language->id
-		);
+    public function renderForm()
+    {
+        $fields_form = array(
+            'form' => array(
+                'legend' => array(
+                    'title' => $this->l('Contact details'),
+                    'icon' => 'icon-envelope'
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'text',
+                        'label' => $this->l('Account owner'),
+                        'name' => 'BANK_WIRE_OWNER',
+                        'required' => true
+                    ),
+                    array(
+                        'type' => 'textarea',
+                        'label' => $this->l('Details'),
+                        'name' => 'BANK_WIRE_DETAILS',
+                        'desc' => $this->l('Such as bank branch, IBAN number, BIC, etc.'),
+                        'required' => true
+                    ),
+                    array(
+                        'type' => 'textarea',
+                        'label' => $this->l('Bank address'),
+                        'name' => 'BANK_WIRE_ADDRESS',
+                        'required' => true
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                )
+            ),
+        );
 
-		return $helper->generateForm(array($fields_form));
-	}
+        $helper = new HelperForm();
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
+        $helper->default_form_language = $lang->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
+        $this->fields_form = array();
+        $helper->id = (int)Tools::getValue('id_carrier');
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'btnSubmit';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false).'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFieldsValues(),
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id
+        );
 
-	public function getConfigFieldsValues()
-	{
-		return array(
-			'BANK_WIRE_DETAILS' => Tools::getValue('BANK_WIRE_DETAILS', Configuration::get('BANK_WIRE_DETAILS')),
-			'BANK_WIRE_OWNER' => Tools::getValue('BANK_WIRE_OWNER', Configuration::get('BANK_WIRE_OWNER')),
-			'BANK_WIRE_ADDRESS' => Tools::getValue('BANK_WIRE_ADDRESS', Configuration::get('BANK_WIRE_ADDRESS')),
-		);
-	}
+        return $helper->generateForm(array($fields_form));
+    }
+
+    public function getConfigFieldsValues()
+    {
+        return array(
+            'BANK_WIRE_DETAILS' => Tools::getValue('BANK_WIRE_DETAILS', Configuration::get('BANK_WIRE_DETAILS')),
+            'BANK_WIRE_OWNER' => Tools::getValue('BANK_WIRE_OWNER', Configuration::get('BANK_WIRE_OWNER')),
+            'BANK_WIRE_ADDRESS' => Tools::getValue('BANK_WIRE_ADDRESS', Configuration::get('BANK_WIRE_ADDRESS')),
+        );
+    }
 }
