3d119c11935c8973546a2c0cfa72cbdb800da82d
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2016-08-24 10:55:04 +0200

    FO: Disable payment return hook according to flag

--- ps_wirepayment.php
+++ ps_wirepayment.php
@@ -205,7 +205,7 @@ class Ps_Wirepayment extends PaymentModule
 
     public function hookPaymentReturn($params)
     {
-        if (!$this->active) {
+        if (!$this->active || !Configuration::get(self::FLAG_DISPLAY_PAYMENT_INVITE)) {
             return;
         }
 
