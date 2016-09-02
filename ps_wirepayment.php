63d9cd42ab13ccdcdec6d3f26736f72450adb1e7
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-02 14:09:01 +0200

    // Version updated to v1.0.3

--- ps_wirepayment.php
+++ ps_wirepayment.php
@@ -46,7 +46,7 @@ class Ps_Wirepayment extends PaymentModule
     {
         $this->name = 'ps_wirepayment';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.2';
+        $this->version = '1.0.3';
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
         $this->author = 'PrestaShop';
         $this->controllers = array('payment', 'validation');
