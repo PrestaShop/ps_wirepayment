f98f53afa1421578ca0679886df09afcc6bf44c9
MathiasReker <mathias@reker.dk>
2018-12-27 22:03:52 +0100

    Indentation type

--- validation.php
+++ validation.php
@@ -37,23 +37,23 @@ $cart = $context->cart;
 $bankwire = Module::getInstanceByName('ps_wirepayment');
 
 if ($cart->id_customer == 0 OR $cart->id_address_delivery == 0 OR $cart->id_address_invoice == 0 OR !$bankwire->active)
-	Tools::redirect('index.php?controller=order&step=1');
+    Tools::redirect('index.php?controller=order&step=1');
 
 // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
 $authorized = false;
 foreach (Module::getPaymentModules() as $module)
-	if ($module['name'] == 'ps_wirepayment')
-	{
-		$authorized = true;
-		break;
-	}
+    if ($module['name'] == 'ps_wirepayment')
+    {
+        $authorized = true;
+        break;
+    }
 if (!$authorized)
-	die($bankwire->getTranslator()->trans('This payment method is not available.', array(), 'Modules.Wirepayment.Shop'));
+    die($bankwire->getTranslator()->trans('This payment method is not available.', array(), 'Modules.Wirepayment.Shop'));
 
 $customer = new Customer((int)$cart->id_customer);
 
 if (!Validate::isLoadedObject($customer))
-	Tools::redirect('index.php?controller=order&step=1');
+    Tools::redirect('index.php?controller=order&step=1');
 
 $currency = $context->currency;
 $total = (float)($cart->getOrderTotal(true, Cart::BOTH));
