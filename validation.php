ab60f28e086c2072599fdfdc69677907f31378a6
MathiasReker <mathias@reker.dk>
2018-12-27 22:05:14 +0100

    Lowercase keywords

--- validation.php
+++ validation.php
@@ -36,7 +36,7 @@ $context = Context::getContext();
 $cart = $context->cart;
 $bankwire = Module::getInstanceByName('ps_wirepayment');
 
-if ($cart->id_customer == 0 OR $cart->id_address_delivery == 0 OR $cart->id_address_invoice == 0 OR !$bankwire->active)
+if ($cart->id_customer == 0 or $cart->id_address_delivery == 0 or $cart->id_address_invoice == 0 or !$bankwire->active)
 	Tools::redirect('index.php?controller=order&step=1');
 
 // Check that this payment option is still available in case the customer changed his address just before the end of the checkout process
