f9b4da2c667b251ef72880a6c40c85641e6aec3e
MathiasReker <mathias@reker.dk>
2018-12-27 22:03:46 +0100

    No blank lines after phpdoc

--- validation.php
+++ validation.php
@@ -27,7 +27,6 @@
 /**
  * @deprecated 1.5.0 This file is deprecated, use moduleFrontController instead
  */
-
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../header.php');
 include(dirname(__FILE__).'/../../init.php');
