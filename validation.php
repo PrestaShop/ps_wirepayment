e2ec1adc26f8fdb1a661fc06181d727612b4bafd
MathiasReker <mathias@reker.dk>
2018-12-27 22:05:01 +0100

    Include

--- validation.php
+++ validation.php
@@ -28,9 +28,9 @@
  * @deprecated 1.5.0 This file is deprecated, use moduleFrontController instead
  */
 
-include(dirname(__FILE__).'/../../config/config.inc.php');
-include(dirname(__FILE__).'/../../header.php');
-include(dirname(__FILE__).'/../../init.php');
+include dirname(__FILE__).'/../../config/config.inc.php';
+include dirname(__FILE__).'/../../header.php';
+include dirname(__FILE__).'/../../init.php';
 
 $context = Context::getContext();
 $cart = $context->cart;
