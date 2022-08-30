dfba0ceb86354e5fd32334ddc2a6fd54ecbc805f
Progi1984 <franck.lefevre@prestashop.com>
2022-08-30 15:50:34 +0200

    The field Reservations Days is not required

--- ps_wirepayment.php
+++ ps_wirepayment.php
@@ -127,11 +127,13 @@ class Ps_Wirepayment extends PaymentModule
                 Tools::getValue(self::FLAG_DISPLAY_PAYMENT_INVITE)
             );
 
+            $fieldReservationDays = Tools::getValue('BANK_WIRE_RESERVATION_DAYS');
+
             if (!Tools::getValue('BANK_WIRE_DETAILS')) {
                 $this->_postErrors[] = $this->trans('Account details are required.', [], 'Modules.Wirepayment.Admin');
             } elseif (!Tools::getValue('BANK_WIRE_OWNER')) {
                 $this->_postErrors[] = $this->trans('Account owner is required.', [], 'Modules.Wirepayment.Admin');
-            } elseif (!Validate::isUnsignedInt(Tools::getValue('BANK_WIRE_RESERVATION_DAYS'))) {
+            } elseif ($fieldReservationDays && !Validate::isUnsignedInt($fieldReservationDays)) {
                 $this->_postErrors[] = $this->trans(
                     'The %field% is invalid. Please enter a positive integer.',
                     [
